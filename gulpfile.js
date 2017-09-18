b7b68e3bf891d127b37c50f3118ce6b2d2bc238b
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2017-09-19 01:00:06 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp')
+
+const css = () => {
+  const sass = require('gulp-sass')
+  const postcss = require('gulp-postcss')
+
+  return gulp.src('test.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([
+      require('postcss-custom-media'),
+      require('postcss-pseudo-class-any-link'),
+      require('postcss-selector-matches'),
+      require('postcss-selector-not'),
+    ]))
+    .pipe(gulp.dest('.'))
+}
+
+exports.css = css
